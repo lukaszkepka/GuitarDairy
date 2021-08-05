5a0414226be6a4ea0f8e14a8d2bd23af8fafffc9
lukaszkepka <lukaszkepka95@gmail.com>
2021-08-05 22:38:15 +0200

    [GD-2] Fluent data generator for Entry entity

--- GuitarDairy.UnitTests/DataGeneration/EntriesDataGenerator.cs
+++ GuitarDairy.UnitTests/DataGeneration/EntriesDataGenerator.cs
@@ -1,21 +1,65 @@
 ﻿using GuitarDairy.Domain.Entities;
+using GuitarDairy.UnitTests.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GuitarDairy.UnitTests.DataGeneration
 {
-    public static class EntriesDataGenerator
+    public class EntryDataGenerator
     {
-        public static IEnumerable<Entry> Generate(Exercise exercise, IEnumerable<Tuple<DateTime, int>> items)
+        private TimeSpan _duration = TimeSpan.FromMinutes(30);
+        private IEnumerable<Exercise> _exercises = Enumerable.Empty<Exercise>();
+        private readonly Dictionary<DateTime, int> _itemsCountPerDate = new();
+
+        public static EntryDataGenerator New()
         {
-            foreach (var item in items)
+            return new EntryDataGenerator();
+        }
+
+        public EntryDataGenerator SetupDuration(TimeSpan duration)
+        {
+            this._duration = duration;
+            return this;
+        }
+
+        public EntryDataGenerator SetupExercises(IEnumerable<Exercise> exercises)
+        {
+            this._exercises = exercises;
+            return this;
+        }
+
+        public EntryDataGenerator SetupExerciseCount(int count)
+        {
+            var exerciseNames = Enumerable.Range(1, count).Select(x => x.ToString()).ToArray();
+            this._exercises = ExerciseDataGenerator.GenerateFromNames(exerciseNames);
+            return this;
+        }
+
+        public EntryDataGenerator SetupItemsCount(DateTime date, int count)
+        {
+            if(! _itemsCountPerDate.TryGetValue(date, out _))
             {
-                for (int i = 0; i < item.Item2; i++)
+                _itemsCountPerDate[date] = 0;
+            }
+
+            _itemsCountPerDate[date] += count;
+            return this;
+        }
+
+        public IEnumerable<Entry> Generate()
+        {
+            var entryInputData = this._exercises
+                .Product(_itemsCountPerDate);
+
+            foreach (var (exercise, itemsPerDate) in entryInputData)
+            {
+                for (int i = 0; i < itemsPerDate.Value; i++)
                 {
                     yield return new Entry()
                     {
-                        Duration = TimeSpan.FromMinutes(30),
-                        Date = item.Item1,
+                        Duration = _duration,
+                        Date = itemsPerDate.Key,
                         Exercise = exercise
                     };
                 }
---
+++ GuitarDairy.UnitTests/Utils/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarDairy.UnitTests.Utils
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<Tuple<T1, T2>> Product<T1, T2>(this IEnumerable<T1> items1, IEnumerable<T2> items2)
+        {
+            foreach (var item1 in items1)
+            {
+                foreach (var item2 in items2)
+                {
+                    yield return Tuple.Create(item1, item2);
+                }
+            }
+        }
+    }
+}
